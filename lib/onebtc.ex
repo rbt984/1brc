949b8fcb135bee98181cf68292755d202fa8b114
Raphaël Barriet <raphael.barriet@tryriot.com>
2026-02-20 09:05:19 +0100

    Initial commit: onebtc project with create_measurements script

---
+++ lib/onebtc.ex
@@ -0,0 +1,18 @@
+defmodule Onebtc do
+  @moduledoc """
+  Documentation for `Onebtc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Onebtc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
